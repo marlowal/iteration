cb241d6620126c8c96a6efeccbc3fe90774ccabb
Amanda Marlow <marlowal@s.dcsdk12.org>
2017-10-24 08:28:36 -0600

    accumulation patterns

--- iteration.py
+++ iteration.py
@@ -1,8 +1,5 @@
-# Make a local change
-# Make another local change
-# Change on github
-
 # iteration pattern
+# doing the same thing once for each member of a list
 
 # [1, 5, 7 ,8 , 4, 3]
 
@@ -15,6 +12,12 @@ def iterate(list):
 	for item in list:
 		print item
 
+def add_one(list):
+	for i in range(0, len(list)):
+		list[i] += 1
+
+	return list
+
 def print_scores(names, scores):
 	for i in range(0, len(names)):
 		print names[i] , " scored " , scores[i]
@@ -25,3 +28,25 @@ def congratulations(names, scores):
 	for i in range(0, len(names)):
 		if (scores[i] == 100):
 			print "Congrats", names[i], "! You got a perfect score!"
+
+#accumulation pattern - type of iteration
+#keep track of other data as we go
+
+def sum(list):
+	total = 0
+	for n in list:
+		total += n
+
+	return total
+
+def max(numbers):
+	current_max = numbers[0]
+	for n in numbers:
+		if n > current_max:
+			current_max = n
+
+	return current_max
+
+# HW 
+	#a funtion - average
+	#b 2nd function also finds average but drops lowest 2 scores
--- test.py
+++ test.py
@@ -1,13 +1,21 @@
 from iteration import *
 
-scores = [100, 90, 85, 100, 0]
+#scores = [100, 90, 85, 100, 0]
 
-iterate(scores)
+#iterate(scores)
 
-names = ["Amy", "Bill", "Carol", "Dave", "Emily"]
+#names = ["Amy", "Bill", "Carol", "Dave", "Emily"]
 
-iterate(names)
+#iterate(names)
 
-print_scores(names, scores)
+#print_scores(names, scores)
 
-congratulations(names, scores)
+#congratulations(names, scores)
+
+#add_one(scores)
+
+scores = [1, 2, 3, 7, 4, 5]
+
+print (sum(scores))
+
+print (max(scores))
